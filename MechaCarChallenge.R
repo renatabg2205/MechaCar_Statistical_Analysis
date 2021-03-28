66b150e64da385f23192889948dc488d353b614b
renatabg2205 <renatabg@gmail.com>
2021-03-27 19:43:40 -0500

    Updating MechaCarChallenge.R and README file and adding Deliverable 3 images.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -23,3 +23,18 @@ total_summary <- suspensionCoil %>% summarize(Mean=mean(PSI),Median=median(PSI),
 #Write an RScript that creates a lot_summary dataframe using the group_by() and the summarize() functions to 
 #group each manufacturing lot by the mean, median, variance, and standard deviation of the suspension coil's PSI column.
 lot_summary <- suspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+#Write an RScript using the t.test() function to determine if the PSI across all manufacturing lots is statistically different 
+#from the population mean of 1,500 pounds per square inch.
+t.test(log10(suspensionCoil$PSI),mu=mean(log10(suspensionCoil$PSI)))
+
+#Write three more RScripts in your MechaCarChallenge.RScript using the t.test() function and its subset() argument to determine 
+#if the PSI for each manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch.
+lot1 <- subset(suspensionCoil, Manufacturing_Lot == "Lot1")
+t.test(log10(lot1$PSI),mu=mean(log10(suspensionCoil$PSI)))
+
+lot2 <- subset(suspensionCoil, Manufacturing_Lot == "Lot2")
+t.test(log10(lot2$PSI),mu=mean(log10(suspensionCoil$PSI)))
+
+lot3 <- subset(suspensionCoil, Manufacturing_Lot == "Lot3")
+t.test(log10(lot3$PSI),mu=mean(log10(suspensionCoil$PSI)))
